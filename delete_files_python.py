f50d5c904d27e35984a01a8d9de4503573f5af0d
eliorsade <108821327+eliorsade@users.noreply.github.com>
2022-08-20 18:10:23 +0300

    Update delete_files_python.py

--- delete_files_python.py
+++ delete_files_python.py
@@ -6,6 +6,12 @@ print(Path.home())
 os.chdir(Path.home())
 os.chdir('old_files')
 mydir = os.getcwd()
+
+
+isExist = os.path.exists(mydir)
+if not isExist:
+  os.makedirs(path)
+
 filelist = [f for f in os.listdir(mydir)]
 for f in filelist:
     os.remove(os.path.join(mydir, f))
