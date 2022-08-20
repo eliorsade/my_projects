43c461f7ba7927a9e71c88753b1c44da6dd31c98
eliorsade <108821327+eliorsade@users.noreply.github.com>
2022-08-20 17:53:29 +0300

    Update delete_files_python.py

--- delete_files_python.py
+++ delete_files_python.py
@@ -1,8 +1,7 @@
 #!/user/bin/python3
 ######
 import os
-mydir = "$HOME"
-value = os.getenv(mydir, default=None)
+mydir = os.path.expanduser('~')
 filelist = [f for f in os.listdir(mydir)]
 for f in filelist:
     os.remove(os.path.join(mydir, f))
